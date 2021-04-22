e36e0fef0b6fadebc8388319c24442a5bcdf13b5
Miguel Fernandez <beisboldom1@gmail.com>
2021-04-22 22:33:13 +0000

    First commit

---
+++ beisboldom/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'sow6c!al&4omsr_bao7u*by2cs4ep-cx1k+%82ffejmsps!hoc'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
